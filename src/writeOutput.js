9c68cbe30a49e0be7d419a0d7451d11b18b2f52d
Ron Kok <ronkok@comcast.net>
2021-04-15 20:16:37 -0700

    Write E with correct units.

--- src/writeOutput.js
+++ src/writeOutput.js
@@ -8,7 +8,7 @@ const loadInitial = ["", "D", "F", "L", "H", "Lr", "S", "R", "W", "E"]
 export function writeOutput(img, input, beam, nodes, extremes) {
   let html = "<h4>Beam Analysis</h4>\n"
   html += `<p><strong>Input</strong>${img}</p>\n`
-  html += "<pre><code>E: " + input.E + (beam.SI ? " ksi" : " MPa") + "\n"
+  html += "<pre><code>E: " + input.E + (beam.SI ? " MPa" : " ksi") + "\n"
   if (input.section && sections[input.section]) { html += "Section: " + input.section + "\n" }
   if (beam.I) {
     const I = beam.SI && input.section && sections[input.section] ? round(beam.I, 3) : beam.I
