ab0002a7a637058fb9fd39bcc9681eab25f08cbe
Ron Kok <ronkok@comcast.net>
2022-05-21 13:29:28 -0700

    Inline CSS and JS in the HTML file.

--- site/beamAnalysis.js
+++ site/beamAnalysis.js
@@ -5907,9 +5907,9 @@ var beam = (function (exports) {
           }
           R = R.slice(0, -2) + "}";
         }
-        R +=  " " + (beam.SI ? "kN" : "kips");
+        R +=  " '" + (beam.SI ? "kN" : "kips") + "'";
         html += `<p>
-  <span class='hurmet-calc' data-entry="R_${i} = '${R.replace(/"/g, "&quot;")}'">R<sub>${i}</sub> = ${R}</span>
+  <span class='hurmet-calc' data-entry="R_${i} = ${R.replace(/"/g, "&quot;")}">R<sub>${i}</sub> = ${R}</span>
 </p>\n`;
       }
       if (Math.abs(nodes[i].Mr[0]) !== 0) {
@@ -5926,9 +5926,9 @@ var beam = (function (exports) {
           }
           M = M.slice(0, -2) + "}";
         }
-        M += " " + (beam.SI ? "kN·m" : "k·ft");
+        M += " '" + (beam.SI ? "kN·m" : "k·ft") + "'";
         html += `<p>
-  <span class='hurmet-calc' data-entry="M_${i} = '${M.replace(/"/g, "&quot;")}'">M<sub>${i}</sub> = ${M}</span>
+  <span class='hurmet-calc' data-entry="M_${i} = ${M.replace(/"/g, "&quot;")}">M<sub>${i}</sub> = ${M}</span>
 </p>\n`;
       }
     }
@@ -5939,30 +5939,30 @@ var beam = (function (exports) {
     // Write the shear absolute maximum
     f = 1 / (beam.SI ? 1000 : 4448.2216152605);
     let V = Math.max(vMax, Math.abs(vMin));
-    V = round(f * V, 3) + " " + (beam.SI ? 'kN' : 'kips');
+    V = round(f * V, 3) + " '" + (beam.SI ? 'kN' : 'kips') + "'";
     html += `<p>
-   <span class='hurmet-calc' data-entry="V_${sub} = '${V}'">V<sub>${sub}</sub> = ${V}</span>
+   <span class='hurmet-calc' data-entry="V_${sub} = ${V}">V<sub>${sub}</sub> = ${V}</span>
 </p>\n`;
 
     // Write the moment maximum
     f = 1 / (beam.SI ? 1000 : 4448.2216152605 * 0.3048);
-    let M = round(f * mMax, 3) + " " + (beam.SI ? 'kN·m' : 'k·ft');
+    let M = round(f * mMax, 3) + " '" + (beam.SI ? 'kN·m' : 'k·ft') + "'";
     html += `<p>
-   <span class='hurmet-calc' data-entry="M_${sub} = '${M}'">M<sub>${sub}</sub> = ${M}</span>
+   <span class='hurmet-calc' data-entry="M_${sub} = ${M}">M<sub>${sub}</sub> = ${M}</span>
 </p>\n`;
 
     // Write the moment minimum.
-    M = round(f * mMin, 3) + " " + (beam.SI ? 'kN·m' : 'k·ft');
+    M = round(f * mMin, 3) + " '" + (beam.SI ? 'kN·m' : 'k·ft') + "'";
     sub = beam.comboName === "service" ? "min" : "uMin";
     html += `<p>
-   <span class='hurmet-calc' data-entry="M_${sub} = '${M}'">M<sub>${sub}</sub> = ${M}</span>
+   <span class='hurmet-calc' data-entry="M_${sub} = ${M}">M<sub>${sub}</sub> = ${M}</span>
 </p>\n`;
 
     if (beam.EI !== 1) {
       f = beam.SI ? 1000 : (12 / 0.3048);
-      const delta = round(f * beam.deflectionMax, 3) + " " + (beam.SI ? 'mm' : 'in');
+      const delta = round(f * beam.deflectionMax, 3) + " '" + (beam.SI ? 'mm' : 'in') + "'";
       html += `<p>
-  <span class='hurmet-calc' data-entry="Δ = '${delta}'">Δ = ${delta}</span>
+  <span class='hurmet-calc' data-entry="Δ = ${delta}">Δ = ${delta}</span>
 </p>`;
     }
 
